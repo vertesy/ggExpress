99ff85fc54029094eb55866947a4550b5d1c3962
Abel Vertesy <vertesy@users.noreply.github.com>
2020-12-16 14:57:18 +0100

    resolves #4

--- ggExpress.functions.R
+++ ggExpress.functions.R
@@ -7,26 +7,77 @@
 require(ggpubr)
 require(cowplot)
 
-# ------------------------------------------------------------------------------------------------
+
+######################################################################
+# Explicit dependencies on MarkdownReportsDev
+######################################################################
+
+# - ...
+
+######################################################################
+# Duplicated functions to avoid dependencies
+######################################################################
+
+
+# MarkdownReportsDev.R
+
+#' unless.specified
+#'
+#' Return value X (TRUE by default) unless the variable is defined.
+#' If defined, it returns the variable.
+#' @param NameOfaVariable Name of a possibly defined variable to be tested.
+#' @param def Default return value
+#' @export
+#' @examples unless.specified("xsadasf32", 2); Num = 22; unless.specified("Num", 1); unless.specified("c", 333)
+
+unless.specified <- function(NameOfaVariable, def = TRUE) {
+  if (exists(NameOfaVariable))
+    get(NameOfaVariable)
+  else
+    def
+}
+
+
+# MarkdownReportsDev.R ------------------------------------------------------------------------------------------------
+#' ww.ttl_field
+#'
+#' Internal function. Creates the string written into the PDF files "Title' (metadata) field.
+#' @param flname Name of the plot
+#' @param creator String X in: "plotblabla by X". Default: "ggExpress".
+#' @export
+#' @examples ww.ttl_field("/Users/myplot.jpg")
+
+ww.ttl_field <- function(plotname, creator = "ggExpress") {
+  paste0(basename(plotname), " by "
+         , unless.specified("b.scriptname", def = creator) )
+}
+
+
+# MarkdownReportsDev.R ------------------------------------------------------------------------------------------------
 #' percentage_formatter
 #'
 #' Parse a string of 0-100% from a number between 0 and 1.
 #' @param x A vector of numbers between 0-1.
 #' @param digitz Number of digits to keep. 3 by default.
 #' @export
-#' @examples percentage_formatter (x = 4.2822212, digitz = 3)
+#' @examples percentage_formatter(x = 4.2822212, digitz = 3)
 
 percentage_formatter <- function(x, digitz = 3) {
-  a = paste(100 * signif (x, digitz), "%", sep = " ")
+  a = paste(100 * signif(x, digitz), "%", sep = " ")
   a[a == "NaN %"] = NaN
   a[a == "NA %"] = NA
   return(a)
 }
 
-# ------------------------------------------------------------------------------------------------
+# MarkdownReportsDev.R ------------------------------------------------------------------------------------------------
 kpp <- function(...) { paste(..., sep = '.', collapse = '.') }
 
-# qqSave ------------------------------------------------------------------------------------------------
+
+######################################################################
+# Original functions
+######################################################################
+
+# ------------------------------------------------------------------------------------------------
 qqSave <- function(ggobj, ext =c("png", "pdf")[1], w =4, h = w
                    , page = c(F, "A4p", "A4l", "A5p", "A5l")[1]
                    , title = F, fname = F, ...) {
@@ -42,7 +93,7 @@ qqSave <- function(ggobj, ext =c("png", "pdf")[1], w =4, h = w
   }
   print(paste0(getwd(),"/", fname))
   # ggsave(plot = ggobj, filename = fname)
-  cowplot::save_plot(plot = ggobj, filename = fname, base_width = w, base_height = h, ...)
+  cowplot::save_plot(plot = ggobj, filename = fname, base_width = w, base_height = h, title = ww.ttl_field(title), ...)
 }
 # qqSave(ggobj = qplot(12))
 
@@ -65,6 +116,7 @@ qqqCovert.named.vec2tbl <- function(namedVec=1:14, verbose = F, strip.too.many.n
 }
 # qqqCovert.named.vec2tbl(namedVec = c("A"=2, "B"=29) )
 
+
 # ------------------------------------------------------------------------------------------------
 qhistogram <-  function(vec, ext = "pdf", xlab = F, vline = F, plot = TRUE, save = TRUE
                         , w = 5, h = w, ...) {
@@ -84,8 +136,8 @@ qhistogram <-  function(vec, ext = "pdf", xlab = F, vline = F, plot = TRUE, save
   if (save) qqSave(ggobj = p, title = plotname, fname = fname, ext = ext, w = w, h = h)
   if (plot) p
 }
-# qhistogram(weight2, vline = 60)
-# qhistogram(weight)
+# weight <- rnorm(1000); qhistogram(weight, vline = 3)
+
 
 
 
