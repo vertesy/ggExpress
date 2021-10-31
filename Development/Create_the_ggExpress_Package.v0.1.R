698cf262fd21c67b750946c2ea00390ff082716c
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-10-31 12:35:29 +0100

    Script to create the package

--- Development/Create_the_ggExpress_Package.v0.1.R
+++ Development/Create_the_ggExpress_Package.v0.1.R
@@ -1,13 +1,12 @@
 ######################################################################################################
-# Workflow_to_Create_an_R_Package.R
-# 01 Jan 2018
+# Create_the_ggExpress_Package.v0.1.R
+# 31 10 2021
 ######################################################################################################
-# source("~/MarkdownReports/Workflow_to_Create_an_R_Package.R")
+# source("/Users/abel.vertesy/GitHub/Packages/ggExpressDev/Development/Create_the_ggExpress_Package.v0.1.R")
 rm(list = ls(all.names = TRUE));
 try(dev.off(), silent = TRUE)
 # install.packages("devtools")
 # Functions ------------------------
-# devtools::install_github(repo = "vertesy/MarkdownReports/MarkdownReports")
 try (source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R'),silent= FALSE)
 
 # irequire("devtools")
@@ -24,36 +23,35 @@ if (print == TRUE) {
 }
 
 # Setup ------------------------
-PackageName = 	"MarkdownReportsDev"
-# PackageName = 	"MarkdownReports"
-setwd("~/GitHub/")
+PackageName = 	"ggExpressDev"
+setwd("~/GitHub/Packages/")
 
 RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
 fname = 	kollapse(PackageName, ".R")
 Package_FnP = 	kollapse(RepositoryDir, "R/", fname)
 
-BackupDir = "~/GitHub/Packages/MarkdownReports/Development/"
+BackupDir = "~/GitHub/Packages/ggExpressDev/Development/"
 dir.create(BackupDir)
 
 # devtools::use_package("vioplot")
-DESCRIPTION <- list("Title" = "Generate Scientific Figures and Reports Easily"
-    , "Author" = person(given = "Abel", family = "Vertesy", email = "a.vertesy@hubrecht.eu", role =  c("aut", "cre") )
-    , "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "a.vertesy@hubrecht.eu", role =  c("aut", "cre") )'
-    , "Description" = "MarkdownReports is a set of R functions that allows you to generate precise figures easily,
-    and create clean markdown reports about what you just discovered with your analysis script. It helps you to:
+DESCRIPTION <- list("Title" = "ggExpress is the fastest way to create, annotate and export plots in R."
+    , "Author" = person(given = "Abel", family = "Vertesy", email = "abel.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )
+    , "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "a.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )'
+    , "Description" = "ggExpress is a set of R functions that allows you to generate precise figures easily,
+    and create clean markdown reports about what you just discovered with your analysis script. It is ggplot-based smaller brother of vertesy/MarkdownReports. It helps you to:
     1. Create scientifically accurate (annotated) figures with very short code, making use of variable-, row- and columnnames.
     2. Save figures automatically as vector graphic (.pdf), that you can use from presentation to posters anywhere.
     3. Incorporate your figures automatically in a markdown report file.
     4. Describe your figures & findings in the same report in a clear and nicely formatted way, parsed from your variables into english sentences.
     5. Share your report, by exporting your report to .pdf, .html or .docx, or via Github or a personal website."
     , "License" = "GPL-3 + file LICENSE"
-    , "Version" = "4.3.2"
+    , "Version" = "0.1.0"
     # , "Version" = "4.0.0"
     , "Packaged" =  Sys.time()
     , "Repository" =  "CRAN"
-    , "Imports" = "stats, methods, sm, graphics, grDevices, gplots, RColorBrewer, colorRamps, clipr, vioplot, VennDiagram, sessioninfo"
+    , "Imports" = "tidyverse, cowplot, ggpubr, stats, methods, sm, graphics, grDevices, gplots, RColorBrewer, sessioninfo, MarkdownReports"
     # , "Suggests" = ""
-    , "BugReports"= "https://github.com/vertesy/MarkdownReports/issues"
+    , "BugReports"= "https://github.com/vertesy/ggExpressDev/issues"
 )
 
 
@@ -61,7 +59,7 @@ setwd(RepositoryDir)
 if ( !dir.exists(RepositoryDir) ) { create(path = RepositoryDir, description = DESCRIPTION, rstudio = TRUE)
 } else {
     getwd()
-    try(file.remove(c("DESCRIPTION","NAMESPACE", "MarkdownReportsDev.Rproj")))
+    try(file.remove(c("DESCRIPTION","NAMESPACE", "ggExpressDev.Rproj")))
     create_package(path = RepositoryDir, fields = DESCRIPTION)
 }
 
@@ -90,8 +88,8 @@ document()
 # Install your package ------------------------------------------------
 # # setwd(RepositoryDir)
 install(RepositoryDir)
-# require("MarkdownReportsDev")
-# # remove.packages("MarkdownReports")
+# require("ggExpressDev")
+# # remove.packages("ggExpressDev")
 # # Test your package ------------------------------------------------
 # help("wplot")
 # cat("\014")
@@ -99,13 +97,13 @@ install(RepositoryDir)
 
 
 # Test if you can install from github ------------------------------------------------
-# devtools::install_github(repo = "vertesy/MarkdownReportsDev")
-# devtools::install_github(repo = "vertesy/MarkdownReports/MarkdownReports")
-# require("MarkdownReports")
+# devtools::install_github(repo = "vertesy/ggExpressDev")
+
+# require("ggExpress")
 
 # Clean up if not needed anymore ------------------------------------------------
 # View(installed.packages())
-# remove.packages("MarkdownReports")
+# remove.packages("ggExpressDev")
 
 check(RepositoryDir, cran = TRUE)
 # as.package(RepositoryDir)
@@ -113,9 +111,9 @@ check(RepositoryDir, cran = TRUE)
 #
 # # source("https://install-github.me/r-lib/desc")
 # # library(desc)
-# # desc$set("MarkdownReports", "foo")
-# # desc$get(MarkdownReports)
+# # desc$set("ggExpress", "foo")
+# # desc$get(ggExpress)
 #
 #
-# system("cd ~/GitHub/MarkdownReportsDev/; ls -a; open .Rbuildignore")
+# system("cd ~/GitHub/ggExpressDev/; ls -a; open .Rbuildignore")
 #
