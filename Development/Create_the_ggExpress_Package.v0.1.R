452cb308841b8cb918a38863620a6fe61c0ae59e
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-11-04 22:29:22 +0100

    small update

--- Development/Create_the_ggExpress_Package.v0.1.R
+++ Development/Create_the_ggExpress_Package.v0.1.R
@@ -40,7 +40,7 @@ DESCRIPTION <- list("Title" = "ggExpress is the fastest way to create, annotate 
     4. Describe your figures & findings in the same report in a clear and nicely formatted way, parsed from your variables into english sentences.
     5. Share your report, by exporting your report to .pdf, .html or .docx, or via Github or a personal website."
     , "License" = "GPL-3 + file LICENSE"
-    , "Version" = "0.1.5"
+    , "Version" = "0.1.6"
     , "Packaged" =  Sys.time()
     , "Repository" =  "CRAN"
     , "Depends" =  "Stringendo"
