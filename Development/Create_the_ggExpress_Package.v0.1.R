434f387ba1eae1e30b65748ee3bc016561613d9d
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-11-04 20:25:04 +0100

    Updates 1. Load packages 2. Check package dependencies

--- Development/Create_the_ggExpress_Package.v0.1.R
+++ Development/Create_the_ggExpress_Package.v0.1.R
@@ -5,22 +5,17 @@
 # source("/Users/abel.vertesy/GitHub/Packages/ggExpress/Development/Create_the_ggExpress_Package.v0.1.R")
 rm(list = ls(all.names = TRUE));
 try(dev.off(), silent = TRUE)
-# install.packages("devtools")
+
 # Functions ------------------------
-try (source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R'),silent= FALSE)
+# install_version("devtools", version = "2.0.2", repos = "http://cran.at.r-project.org") # install.packages("devtools")
+require("devtools")
+require("roxygen2")
+require("stringr")
 
-# irequire("devtools")
-# install_version("devtools", version = "2.0.2", repos = "http://cran.at.r-project.org")
-irequire("devtools")
-irequire("roxygen2")
-irequire("stringr")
+# devtools::install_github(repo = "vertesy/CodeAndRoll2")
+require('CodeAndRoll2')
+require('Stringendo')
 
-kollapse <-function(..., print = TRUE) {
-if (print == TRUE) {
-    print(paste0(c(...), collapse = ""))
-  }
-  paste0(c(...), collapse = "")
-}
 
 # Setup ------------------------
 PackageName = 	"ggExpress"
@@ -48,6 +43,7 @@ DESCRIPTION <- list("Title" = "ggExpress is the fastest way to create, annotate 
     , "Version" = "0.1.5"
     , "Packaged" =  Sys.time()
     , "Repository" =  "CRAN"
+    , "Depends" =  "Stringendo"
     , "Imports" = "tidyverse, cowplot, ggpubr, stats, methods, sm, graphics, grDevices, RColorBrewer, sessioninfo, MarkdownReports"
     # , "Suggests" = ""
     , "BugReports"= "https://github.com/vertesy/ggExpress/issues"
@@ -116,3 +112,14 @@ check(RepositoryDir, cran = TRUE)
 #
 # system("cd ~/GitHub/ggExpress/; ls -a; open .Rbuildignore")
 #
+# Check package dependencies ------------------------------------------------
+depFile = paste0(RepositoryDir, 'Development/Dependencies.R')
+
+(f.deps <- NCmisc::list.functions.in.file(filename = Package_FnP))
+# clipr::write_clip(f.deps)
+
+sink(file = depFile); print(f.deps); sink()
+p.deps <- gsub(x = names(f.deps), pattern = 'package:', replacement = '')
+write(x = p.deps, file = depFile, append = T)
+
+
---
+++ Development/Dependencies.R
@@ -0,0 +1,20 @@
+$`character(0)`
+ [1] "element_text"            "geom_hline"              "geom_vline"             
+ [4] "ggbarplot"               "ggdensity"               "gghistogram"            
+ [7] "ggpar"                   "ggpie"                   "ggscatter"              
+[10] "grids"                   "NoLegend"                "qMarkdownImageLink"     
+[13] "qqqAxisLength"           "qqqCovert.named.vec2tbl" "qqqParsePlotname"       
+[16] "qqSave"                  "scale_x_log10"           "scale_y_log10"          
+[19] "theme"                  
+
+$`package:base`
+ [1] "as.character" "c"            "colnames"     "factor"       "isFALSE"     
+ [6] "length"       "paste"        "print"        "rep"          "signif"      
+[11] "substitute"   "sum"          "unique"      
+
+$`package:Stringendo`
+[1] "flag.nameiftrue" "kpp"            
+
+character(0)
+base
+Stringendo
