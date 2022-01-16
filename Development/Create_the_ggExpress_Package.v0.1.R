94f432fdda968fb0b9eeb7d4bb5881912dada59d
vertesy <abel.vertesy@imba.oeaw.ac.at>
2022-01-16 18:57:47 +0100

    v0.3.6

--- Development/Create_the_ggExpress_Package.v0.1.R
+++ Development/Create_the_ggExpress_Package.v0.1.R
@@ -19,7 +19,7 @@ require('Stringendo')
 
 # Setup ------------------------
 PackageName = "ggExpress"
-package.version = "0.3.5"
+package.version = "0.3.6"
 setwd("~/GitHub/Packages/")
 
 RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
