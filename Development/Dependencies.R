a6e7cc7fa59d5310258553ce9c91e9905e568ef9
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 00:06:44 +0100

    dependencies

--- Development/Dependencies.R
+++ Development/Dependencies.R
@@ -1,32 +1,44 @@
+Dependency file generated on Mon Nov 27 00:06:14 2023 
+
+#################################################################################################### 
+ggExpress.R
+#################################################################################################### 
 $`character(0)`
-[1] "as_tibble" "save_plot"
+[1] "as_tibble"     "ggVennDiagram" "iround"        "is.list2"      "plot_grid"     "save_plot"    
 
 $`package:base`
- [1] "as.character" "c"            "cbind"        "colnames"    
- [5] "factor"       "getwd"        "is.null"      "isFALSE"     
- [9] "length"       "make.names"   "max"          "names"       
-[13] "paste"        "paste0"       "print"        "rep"         
-[17] "return"       "round"        "signif"       "substitute"  
-[21] "sum"          "unique"      
+ [1] "as.character"  "as.data.frame" "as.numeric"    "c"             "cat"           "cbind"         "colnames"     
+ [8] "factor"        "getwd"         "is.matrix"     "is.null"       "is.numeric"    "isFALSE"       "length"       
+[15] "make.unique"   "max"           "names"         "ncol"          "paste"         "paste0"        "print"        
+[22] "rep"           "rev"           "round"         "signif"        "sort"          "stopifnot"     "substitute"   
+[29] "sum"           "unique"        "unlist"        "which"        
 
 $`package:ggExpress`
-[1] "qMarkdownImageLink"      "qqqAxisLength"          
-[3] "qqqCovert.named.vec2tbl" "qqqParsePlotname"       
-[5] "qqSave"                 
+[1] "qMarkdownImageLink"  "qqqAxisLength"       "qqqList.2.DF.ggplot" "qqqNamed.Vec.2.Tbl"  "qqSave"             
 
 $`package:ggplot2`
-[1] "element_text"  "geom_hline"    "geom_vline"    "scale_x_log10"
-[5] "scale_y_log10" "theme"        
+ [1] "annotation_logticks" "coord_polar"         "element_rect"        "element_text"        "geom_abline"        
+ [6] "geom_density_2d"     "geom_hline"          "geom_vline"          "guides"              "labs"               
+[11] "scale_fill_gradient" "scale_x_log10"       "scale_y_log10"       "theme"              
 
 $`package:ggpubr`
-[1] "ggbarplot"   "ggdensity"   "gghistogram" "ggpar"       "ggpie"      
-[6] "ggscatter"   "grids"      
+ [1] "ggbarplot"          "ggboxplot"          "ggdensity"          "gghistogram"        "ggpar"             
+ [6] "ggpie"              "ggscatter"          "ggstripchart"       "ggviolin"           "grids"             
+[11] "stat_compare_means" "stat_cor"          
 
 $`package:MarkdownHelpers`
-[1] "llogit"
+[1] "llogit"                "stopif"                "unless.specified"      "ww.FnP_parser"        
+[5] "ww.set.file.extension" "ww.ttl_field"         
+
+$`package:stats`
+[1] "median"
 
 $`package:Stringendo`
-[1] "flag.nameiftrue" "kpp"            
+[1] "FixPlotName"          "flag.nameiftrue"      "iprint"               "percentage_formatter"
+[5] "sppp"                
+
+$`package:utils`
+[1] "stack"
 
 character(0)
 base
@@ -34,4 +46,6 @@ ggExpress
 ggplot2
 ggpubr
 MarkdownHelpers
+stats
 Stringendo
+utils
