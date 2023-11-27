056e045e1a6c7d72c504bbb201fd94675c678700
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 20:55:15 +0100

    order

--- Development/config.R
+++ Development/config.R
@@ -10,12 +10,13 @@ DESCRIPTION <- list(
     3. Incorporate your figures automatically in a markdown report file.
     4. Describe your figures & findings in the same report in a clear and nicely formatted way, parsed from your variables into english sentences.
     5. Share your report, by exporting your report to .pdf, .html or .docx, or via Github or a personal website.",
+
+  depends = "Stringendo, MarkdownHelpers, ggplot2, ggpubr",
+  imports = "cowplot, tibble, RColorBrewer, MarkdownReports, CodeAndRoll2, ggVennDiagram",
+
   author.given = "Abel",
   author.family = "Vertesy",
   author.email = "av@imba.oeaw.ac.at",
   license = "GPL-3 + file LICENSE",
-  depends = "Stringendo, MarkdownHelpers, ggplot2, ggpubr",
-  imports = "cowplot, tibble, RColorBrewer, MarkdownReports, CodeAndRoll2, ggVennDiagram",
   github.user = "vertesy"
 )
-
